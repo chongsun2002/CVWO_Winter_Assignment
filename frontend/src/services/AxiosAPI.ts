ffbbf29695dfe3e921ec3f378e85c44ddd2479dc
chongsun2002 <119829137+chongsun2002@users.noreply.github.com>
2024-01-25 22:35:50 +0800

    replaced localhost with live backend in axios

--- frontend/src/services/AxiosAPI.ts
+++ frontend/src/services/AxiosAPI.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios'
 
-const baseURL = 'http://localhost:8000/v1';
+const baseURL = 'https://cvwo-winter-assignment-zske.onrender.com/v1';
 
 const http: AxiosInstance = axios.create({
     baseURL: baseURL, 
